e17b58deacc78e345a47c552531a750d204ce3ee
Ray Deaver <125383656+rdeaver13@users.noreply.github.com>
2023-04-03 14:22:07 -0500

    Add files via upload
    I unzipped the GP2 folder and committed the files to the repository. I think that adding /GP2/ before the file names make the files recognize them as being part of a package. I'll do more testing to determine if this is correct.

---
+++ GP2/GP2.java
@@ -0,0 +1,79 @@
+package GP2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class GP2 {
+    
+    public GP2() {
+        // Declate Vars
+        int user_input;
+        Scanner input = new Scanner(System.in);
+
+        File myFile = new File("GP2/intro.txt");
+        
+
+        // Print Welcome Introduction
+        try {
+
+            Scanner textFile = new Scanner(myFile);
+            // Print introduction
+            while(textFile.hasNextLine()){
+                System.out.println(textFile.nextLine());
+            }
+            // Close text file
+            textFile.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + e.getMessage());
+        }
+
+        // Looking for user input
+        user_input = input.nextInt();
+
+        // Main Loop
+        while (user_input != 0){
+            
+            switch(user_input) {
+
+                default:
+                // change text file to error message
+                    myFile = new File("GP2/fail.txt");
+                    break;
+
+                case 1: 
+                // change text file to risk factors
+                    myFile = new File("GP2/riskFactors.txt");
+                    break;
+
+                case 2:
+                // change text file to prevention techniques
+                    myFile = new File("GP2/preventionTechniques.txt");
+                    break;
+
+            }
+
+            try {
+                Scanner textFile = new Scanner(myFile);
+                // Print info
+                while(textFile.hasNextLine()){
+                    System.out.println(textFile.nextLine());
+                }
+                // Close text file
+                textFile.close();
+    
+            } catch (FileNotFoundException e) {
+                System.out.println("File not found: " + e.getMessage());
+            }
+
+            // Looking for user input
+            user_input = input.nextInt();
+
+        }
+
+        // Close user input
+        input.close();
+    }
+
+}
---
+++ GP2/Main.java
@@ -0,0 +1,9 @@
+package GP2;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        GP2 gp2 = new GP2();
+
+    }
+}
