d798dc5553dd20c9343bb5b5c998ba79d0a4f8d5
Ray Deaver <125383656+rdeaver13@users.noreply.github.com>
2023-04-05 18:12:06 +0000

    Changes were made

--- GP2/Main.java
+++ GP2/Main.java
@@ -3,7 +3,15 @@ package GP2;
 public class Main {
     public static void main(String[] args) {
         
-        GP2 gp2 = new GP2();
+        //GP2 gp2 = new GP2();
+
+        // start preventionTechniques
+
+        preventionTechniques preventionTechniques = new preventionTechniques();
+
+        preventionTechniques.runPreventionTechniques();
+
+        // end preventionTechniques
 
     }
 }
