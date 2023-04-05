9f63975e8751cd7a86bdab993ebe1154759996fd
BeterMain <106632392+BeterMain@users.noreply.github.com>
2023-04-05 18:29:16 +0000

    Deleted the zip file and made just added whitespace

--- GP2/GP2.java
+++ GP2/GP2.java
@@ -58,7 +58,7 @@ public class GP2 {
                     break;
                 // End of edit
             }
-
+            
             try {
                 Scanner textFile = new Scanner(myFile);
                 // Print info
