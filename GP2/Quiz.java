60891f0373a65a723af8c8f6b20577b7b5fb9426
mglavosek <115747692+mglavosek@users.noreply.github.com>
2023-04-07 17:02:24 +0000

    Starting the Quiz using the file IO

--- GP2/Quiz.java
+++ GP2/Quiz.java
@@ -1,79 +1,39 @@
 package QuizProject;
 
 import java.util.Scanner;
+import java.awt.*;
+import java.io.*;
+
 
 public class Quiz {
     public static void main(String[] args){
-        //Declaring the arrays
-        String QnA[][] = new String[10][5];
-        String CorrectAns[] = new String[10];
-
-        //Declaring variables
-        String UserAns;
-
         //Declaring the Scanner class
         Scanner input = new Scanner(System.in);
+        
+        //Opening the files
+        try{
+            //Creating the reader for questions.txt and answers.txt
+            FileReader Qreader = new FileReader("Questions.txt");
+            BufferedReader QbufferedReader = new BufferedReader(Qreader);
 
-        //Adding the questions and answers
-        QnA[0][0] = "What is Harrassment?";
-        QnA[0][1] = "Bullying Someone";
-        QnA[0][2] = "Being Nice";
-        QnA[0][3] = "Poking Them";
-        QnA[0][4] = "Eating with Them";
+            FileReader Areader = new FileReader("Answers.txt");
+            BufferedReader AbufferedReader = new BufferedReader(Areader);
 
-        QnA[1][0] = "What is CyberBullying?";
-        QnA[1][1] = "Bullying Online";
-        QnA[1][2] = "Bullying the Internet";
-        QnA[1][3] = "Using the Web";
-        QnA[1][4] = "Fighting in Person";
+            String line;
 
-        QnA[2][0] = "What is an example of bullying?";
-        QnA[2][1] = "All of the above";
-        QnA[2][2] = "Teasing";
-        QnA[2][3] = "Threatening";
-        QnA[2][4] = "Hitting";
+            //Reading and printing all the questions
+            While((line = QbufferedReader.readLine()) != null){
+                System.out.println(line);
+            }
+            Qreader.close();
+            Areader.close();
 
-        QnA[3][0] = "What if I am not comfortable talking to my parents about the bully?";
-        QnA[3][1] = "Do nothing";
-        QnA[3][2] = "Keep it to yourself";
-        QnA[3][3] = "Talk to someone you trust";
-        QnA[3][4] = "Talk to the bully";
+        } catch(IOException e){
+            e.printStackTrace();
+        }
 
-        QnA[4][0] = "At what age does CyberBullying happen most often?";
-        QnA[4][1] = "College age";
-        QnA[4][2] = "High School age";
-        QnA[4][3] = "Middle School age";
-        QnA[4][4] = "Elementary School age";
+        
 
-        QnA[5][0] = "Kids that are Cyberbullied are more likely to...";
-        QnA[5][1] = "Do Drugs";
-        QnA[5][2] = "All of the above";
-        QnA[5][3] = "Skip school";
-        QnA[5][4] = "Have poor grades";
-
-        QnA[6][0] = "What is the most common means of cyberbullying?";
-        QnA[6][1] = "Video Games";
-        QnA[6][2] = "Hand-Written notes";
-        QnA[6][3] = "Cell Phones";
-        QnA[6][4] = "Computers";
-
-        QnA[7][0] = "Should I notify my parents?";
-        QnA[7][1] = "Maybe";
-        QnA[7][2] = "No";
-        QnA[7][3] = "Definitely not";
-        QnA[7][4] = "Yes";
-
-        QnA[8][0] = "Which of the following is NOT the correct way to handle Cyberbullying?";
-        QnA[8][1] = "Ignore or block the person";
-        QnA[8][2] = "Fight the person";
-        QnA[8][3] = 
-        QnA[8][4] =
-
-        QnA[9][0] =
-        QnA[9][1] =
-        QnA[9][2] =
-        QnA[9][3] =
-        QnA[9][4] = "hdkwqhlekqh"
         
     }
 }
