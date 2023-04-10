2736ae04304d6703198f827fba4bee8d56383751
BeterMain <106632392+BeterMain@users.noreply.github.com>
2023-04-10 18:23:04 +0000

    Called GP2 Class in Main.java

--- GP2/Main.java
+++ GP2/Main.java
@@ -3,13 +3,13 @@ package GP2;
 public class Main {
     public static void main(String[] args) {
         
-        //GP2 gp2 = new GP2();
+        GP2 gp2 = new GP2();
 
         // start preventionTechniques
 
-        preventionTechniques preventionTechniques = new preventionTechniques();
+        //preventionTechniques preventionTechniques = new preventionTechniques();
 
-        preventionTechniques.runPreventionTechniques();
+        //preventionTechniques.runPreventionTechniques();
 
         // end preventionTechniques
 
