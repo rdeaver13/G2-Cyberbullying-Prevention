ba16e31804c85a2c2e0f811cae766afdc0919cc6
mglavosek <115747692+mglavosek@users.noreply.github.com>
2023-04-14 12:27:32 -0500

    Update

--- GP2/Quiz.java
+++ GP2/Quiz.java
@@ -83,7 +83,7 @@ public class Quiz {
     }
 
 
-    public void runQuiz() {
+    public static void runQuiz() {
         //Adding the file paths
         String questionsFilePath = "GP2/Questions.txt";
         String answersFilePath = "GP2/Answers.txt";
